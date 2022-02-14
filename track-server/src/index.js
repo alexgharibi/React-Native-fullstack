8ccb8985065ec4445d1bb1f32a1dfc2bf6bf3c70
alexgharibi <ali52346@gmail.com>
2022-02-14 08:35:41 -0500

    add post routes

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
 
 const mongoose = require("mongoose");
 
 const app = express();
+app.use(bodyParser.json());
+app.use(authRoutes);
 
 const mongoUri =
   "mongodb+srv://admin:ottawa123@cluster0.nrsa1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
