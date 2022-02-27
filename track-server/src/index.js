bfd5d08ad2ee902e36d1fcbb21abc73f36aae137
alexgharibi <ali52346@gmail.com>
2022-02-27 13:14:17 -0500

    track routes

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,7 +1,9 @@
 require("./models/User");
+require("./models/Track");
 const express = require("express");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
 const requireAuth = require("./middlewares/requireAuth");
 
 const mongoose = require("mongoose");
@@ -9,6 +11,7 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri =
   "mongodb+srv://admin:ottawa123@cluster0.nrsa1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
