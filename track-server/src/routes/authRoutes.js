6eb9503c968d10c60e88f8d69cf85238db5e876e
alexgharibi <ali52346@gmail.com>
2022-02-27 12:46:22 -0500

    Create a hash and salt logic

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -22,4 +22,25 @@ router.post("/signup", async (req, res) => {
   }
 });
 
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: "Must provide email and password" });
+  }
+
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    return res.status(404).send({ error: "email not found" });
+  }
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, "MY_SECERT_KEY");
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: "invalid password or email" });
+  }
+});
+
 module.exports = router;
